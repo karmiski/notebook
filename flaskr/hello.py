5dce17898603ae316f2493700a3cc5d3a754b3d8
karmiski <karolinamiski@gmail.com>
2023-05-31 22:56:00 +0200

    cleaning

--- flaskr/hello.py
+++ flaskr/hello.py
@@ -1,8 +1,5 @@
 from flask import Flask, render_template, url_for
 app = Flask(__name__)
 @app.route("/")
-def main():
-    return render_template('index.html')
-
-if __name__ == "__main__":
-    app.run(debug=True)
+def hello():
+    return 'h'
